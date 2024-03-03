5e83ca08237fcf70873fe4322f78c84d0e7e79df
beit <beit46@mac.com>
2024-03-03 20:44:00 +0100

    Testing: Ignore playwright testing to ensure jest runs properly from command line

--- jest.config.ts
+++ jest.config.ts
@@ -3,6 +3,7 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/test/setupTests.ts'],
+  "testPathIgnorePatterns": ['tests', 'dist'],
   moduleNameMapper: {
     '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
     '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/test/svg.ts',
